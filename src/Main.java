959515005f68efddb1169e27f7faf0cff7a0899e
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-10-30 09:20:43 -0400

    tggre

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.printf("%-4s", "*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j <= 5; j++) {
+                System.out.printf("%-4s", "*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+        for (int i = 5; i >= 1; i--) {
+            for (int j = i; j <= 5; j++) {
+                System.out.printf("%-4s", "*");
+            }
+            System.out.println("");
+        }
     }
 }
